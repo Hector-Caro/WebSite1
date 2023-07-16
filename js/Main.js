d877ac6b639697c9b1a642f4e546fa9b2bfe6f0f
Hector-Caro <139716303+Hector-Caro@users.noreply.github.com>
2023-07-16 15:30:22 -0500

    Commit Inicial

---
+++ js/Main.js
@@ -0,0 +1,12 @@
+function suma(){
+    let a = 0;
+    let b = 0;
+    let suma = 0;
+
+    a = parseInt(prompt('Por favor ingrese el primer valor'));
+    b = parseInt(prompt('Por favor ingrese el segundo valor'));
+
+    suma = a + b;
+
+    alert("la suma es igual a: " + suma)
+}
